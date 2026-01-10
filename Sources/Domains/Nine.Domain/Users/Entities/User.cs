10999dcfd80a03c39358afa6e6a0d6995608b127
Behnam Salehi <behnam.dev@outlook.com>
2026-01-11 00:04:59 +0330

    Add `User`

---
+++ Sources/Domains/Nine.Domain/Users/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using Nine.Domain.Abstractions.AggregateRoots;
+
+namespace Nine.Domain.Users.Entities;
+
+public sealed class User : EventSourcedAggregateRoot
+{
+    private User(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public string FirstName { get; private set; }
+
+    public string LastName { get; private set; }
+}
