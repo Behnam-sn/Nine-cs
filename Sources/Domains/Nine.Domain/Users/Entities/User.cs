7c0b0a097db02d05b94eb46729d256fa84f2a5a0
Behnam Salehi <behnam.dev@outlook.com>
2026-01-11 00:19:52 +0330

    Add `Create` method to `User`

--- Sources/Domains/Nine.Domain/Users/Entities/User.cs
+++ Sources/Domains/Nine.Domain/Users/Entities/User.cs
@@ -1,16 +1,36 @@
 ﻿using Nine.Domain.Abstractions.AggregateRoots;
+using Nine.Domain.Abstractions.Events;
+using Nine.Domain.Abstractions.ValueObjects;
+using Nine.Domain.Users.Events;
 
 namespace Nine.Domain.Users.Entities;
 
 public sealed class User : EventSourcedAggregateRoot
 {
-    private User(string firstName, string lastName)
+    public User()
     {
-        FirstName = firstName;
-        LastName = lastName;
     }
 
     public string FirstName { get; private set; }
 
     public string LastName { get; private set; }
+
+    private void Apply(UserCreatedDomainEventV1 domainEvent)
+    {
+        FirstName = domainEvent.FirstName;
+        LastName = domainEvent.LastName;
+    }
+
+    public static User Create(string firstName, string lastName)
+    {
+        var user = new User();
+        var userCreatedEvent = new UserCreatedDomainEventV1(
+            Id: DomainEventId.Create(),
+            FirstName: firstName,
+            LastName: lastName,
+            OccurredAt: DateTime.UtcNow
+        );
+        user.RaiseDomainEvent(userCreatedEvent);
+        return user;
+    }
 }
---
+++ Sources/Domains/Nine.Domain/Users/Events/UserCreatedDomainEventV1.cs
@@ -0,0 +1,7 @@
+﻿using Nine.Domain.Abstractions.Events;
+using Nine.Domain.Abstractions.ValueObjects;
+
+namespace Nine.Domain.Users.Events;
+
+public sealed record UserCreatedDomainEventV1(DomainEventId Id, string FirstName, string LastName, DateTime OccurredAt)
+    : IDomainEvent;
