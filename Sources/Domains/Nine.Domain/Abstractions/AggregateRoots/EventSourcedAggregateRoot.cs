f18b5af136f3bb32c66b73fb9f88d9c2fa61717c
Behnam Salehi <behnam.dev@outlook.com>
2026-01-11 00:04:34 +0330

    Add `EventSourcedAggregateRoot`

---
+++ Sources/Domains/Nine.Domain/Abstractions/AggregateRoots/EventSourcedAggregateRoot.cs
@@ -0,0 +1,20 @@
+﻿using Nine.Domain.Abstractions.Events;
+
+namespace Nine.Domain.Abstractions.AggregateRoots;
+
+public abstract class EventSourcedAggregateRoot : IAggregateRoot
+{
+    private readonly List<IDomainEvent> _domainEvents = [];
+
+    public IEnumerable<IDomainEvent> DomainEvents => _domainEvents;
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+}
